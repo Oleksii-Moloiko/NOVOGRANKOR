bf8f33e8c953c81c06918444464f505075b97c06
Oleksii-Moloiko <oleksiimoloiko@gmail.com>
2026-06-17 21:26:57 +0300

    Prepare media storage for Railway deployment

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = config("MEDIA_ROOT", default=str(BASE_DIR / "media"))
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
