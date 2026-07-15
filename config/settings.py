3e8c323da09148f5821b88002e0306680df4fe48
Oleksii-Moloiko <oleksiimoloiko@gmail.com>
2026-07-15 21:53:36 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -11,11 +11,10 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config(
-    "ALLOWED_HOSTS",
-    default="127.0.0.1,localhost",
-    cast=Csv(),
-)
+ALLOWED_HOSTS = [
+    "www.novogrankor.com",
+    "novogrankor.com",
+]
 
 CSRF_TRUSTED_ORIGINS = config(
     "CSRF_TRUSTED_ORIGINS",
