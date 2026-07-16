ef294070e128614a324a3a3374542ebd809e7708
Oleksii-Moloiko <oleksiimoloiko@gmail.com>
2026-07-16 12:04:22 +0300

    fix

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
 
 from .views import home, healthcheck
-from django.views.generic import TemplateView
-
+from django.http import HttpResponse
 
 urlpatterns = [
     path("", home, name="home"),
     path("healthcheck/", healthcheck, name="healthcheck"),
+
     path(
         "robots.txt",
-        TemplateView.as_view(
-            template_name="robots.txt",
+        lambda request: HttpResponse(
+            "User-agent: *\nAllow: /",
             content_type="text/plain",
         ),
+        name="robots",
     ),
-]   
+]
