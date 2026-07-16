f80427c4fc3881adae941ce1bd4a3153bf1faec5
Oleksii-Moloiko <oleksiimoloiko@gmail.com>
2026-07-16 11:35:40 +0300

    dodav robots

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
 from .views import home, healthcheck
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path("", home, name="home"),
     path("healthcheck/", healthcheck, name="healthcheck"),
-]
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt",
+            content_type="text/plain",
+        ),
+    ),
+]   
